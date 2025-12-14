4c857ef39140729c564e9651fc72e7204a276a28
sukhpreetsvirdi <sukhpreetsvirdi@gmail.com>
2025-12-14 17:23:03 +0530

    Add admin categories panel

--- netlify/functions/admin-delete-category.js
+++ netlify/functions/admin-delete-category.js
@@ -5,7 +5,7 @@ export async function handler(event) {
 
   const client = new Client({ connectionString: process.env.NETLIFY_DATABASE_URL });
   await client.connect();
-  await client.query("DELETE FROM products WHERE id=$1", [id]);
+  await client.query("DELETE FROM categories WHERE id=$1", [id]);
   await client.end();
 
   return { statusCode: 200, body: "Deleted" };
